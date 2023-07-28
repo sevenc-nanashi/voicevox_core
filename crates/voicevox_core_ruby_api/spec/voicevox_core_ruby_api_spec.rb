bf108d2fc8e87cdb2f85f814d31418d981bbb2b4
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-07-28 16:43:28 +0900

    Change: bundle gem --ext rustの内容に

---
+++ crates/voicevox_core_ruby_api/spec/voicevox_core_ruby_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe VoicevoxCoreRubyApi do
+  it "has a version number" do
+    expect(VoicevoxCoreRubyApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
